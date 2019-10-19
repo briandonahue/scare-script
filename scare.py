68248ec179b475844d07be4213ff60edc148eeb9
Brian Donahue <brian@pigeonmoon.com>
2019-10-19 17:17:09 -0400

    Add command line args

--- scare.py
+++ scare.py
@@ -3,16 +3,28 @@ import time
 import subprocess
 import os
 import random
+import argparse
 
+parser = argparse.ArgumentParser(description='Scare script!')
+parser.add_argument('--roam', '-r', help='Timer for roaming ghost to appear. Default: 60. Set to 0 to disable.', \
+                    default=60, type=int)
+
+parser.add_argument('--scare', '-s', help='Cooldown for scare ghosts. Default=30. Set to 0 to disable.', \
+                    default=30, type=int)
+args = parser.parse_args()
+
+print args.roam
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)        
 
 Last_State = 0
 Current_State = 0
-Timer_Seconds = 60
+Haunt_Cooldown = args.roam
+Scare_Cooldown = args.scare
 Current_Time = time.time()
-Trigger_Time = Current_Time + 30
+Scare_Window = Current_Time
+Haunt_Trigger = Current_Time + Haunt_Cooldown
 random.seed(time.time())
 
 def startle():
@@ -31,18 +43,23 @@ while True:
 
     Current_State=GPIO.input(11)
     Current_Time = time.time()
-    print "Timer: ", round(Trigger_Time - Current_Time, 2), " Motion: ", Current_State
+    print "Motion: ", Current_State
+    print "Haunt Countdown: ", round(Haunt_Trigger - Current_Time, 2)
+    print "Scare Cooldown: ", round(Scare_Window - Current_Time, 2)
 
 
-    if Current_Time > Trigger_Time:
+    if Current_Time > Haunt_Trigger and Haunt_Cooldown > 0:
         print "Timer Elapsed!"
         roam()
-        Trigger_Time = time.time() + Timer_Seconds
+        Haunt_Trigger = time.time() + Haunt_Cooldown
 
-    elif Current_State==1 and Current_State != Last_State:
+    elif Current_State == 1 and \
+         Current_Time > Scare_Window and \
+         Scare_Cooldown > 0:
         print "Motion Detected", Current_State
         startle()
-        Trigger_Time = time.time() + Timer_Seconds
+        Haunt_Trigger = time.time() + Haunt_Cooldown
+        Scare_Window = time.time() + Scare_Cooldown
 
     time.sleep(.5)
 
