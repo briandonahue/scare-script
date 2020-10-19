75eb17535054236fe15a488aa1b35d6ab42a2d10
Brian Donahue <brian@pigeonmoon.com>
2020-10-19 13:55:45 -0400

    changes

--- server/src/play-video.js
+++ server/src/play-video.js
@@ -9,13 +9,13 @@ const play = async (file) => {
 /*
     const {error, stdout, stderr} = await exec(`sudo omxplayer -b -o both --vol -1000 ${file}`)
     console.log(error, stdout, stderr)
+*/
     if(player) {
         player.newSource(file, 'both', false, 100)
     }
     else {
         player = Omx(file, 'both', false, 100)
     }
-*/
 }
 
 export { play }
